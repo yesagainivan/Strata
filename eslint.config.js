e6b02745bdcde08f71c1c7d64f35cbc4d14b401b
yesagainivan <brandonowono@gmail.com>
2025-12-15 01:21:17 -0500

    ancestors

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
+// 
 export default defineConfig([
   globalIgnores(['dist']),
   {
